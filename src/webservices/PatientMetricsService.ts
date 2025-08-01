49618928d2d0753d23c761d6770dd864ecd9bc05
drindalipi_dd <drin.dalipi@i2invest.ch>
2025-08-01 16:37:40 +0200

    Use HARDCODED_JWT_TOKEN for JWT_TOKEN

--- src/webservices/PatientMetricsService.ts
+++ src/webservices/PatientMetricsService.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 const BASE_URL = "http://127.0.0.1:8000";
 
 // Hardcoded JWT token. Change the value with the token that you get from `/auth` endpoint.
-const JWT_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzU0MDU4OTMxfQ.ZP49qQa5VWV80LengQI-Upml7qXfWV9kpzJ6QMZVao0";
+const JWT_TOKEN = "HARDCODED_JWT_TOKEN";
 
 /**
  * Fetches patient metrics from the backend API.
